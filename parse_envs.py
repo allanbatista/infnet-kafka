33022d9afa51e166fea77a8e4bfa42b41b05a595
Allan Batista <allan@allanbatista.com.br>
2019-12-14 09:40:23 -0300

    start project

---
+++ parse_envs.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+import re
+from os import environ
+
+envvars = {}
+
+startswith = re.compile("^[\w]+\.")
+with open("{}/config/server.properties".format(environ['KAFKA_HOME']), "r") as f:
+    for line in f:
+        if startswith.match(line) is not None:
+            key, value = line.strip().split("=", 1)
+            envvars[key] = value
+
+for key in environ:
+    if key.startswith("kafka."):
+        envvars[key.replace("kafka.", "")] = environ[key]
+
+if environ.get('KAFKA_LOG_DIR'):
+    envvars['log.dir'] = environ.get('KAFKA_LOG_DIR')
+
+
+with open("{}/config/server-generated.properties".format(environ['KAFKA_HOME']), "w+") as f:
+    for key in envvars:
+        f.write("{}={}\n".format(key, envvars[key]))
